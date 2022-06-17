b8d3db18f0e67f9a599157a67875d203d208b47a
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-06-17 19:10:31 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Menubar from "./components/Menubar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <div>
       <Menubar />
+      <Menu />
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,37 @@
+import i18next, { t } from "i18next";
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+
+const Menu = () => {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">{t("Logo")}</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">{t("Menu_Item")}</Nav.Link>
+              <Nav.Link href="#link">{t("Menu_Item_plural")}</Nav.Link>
+              <NavDropdown title={t("Dropdown")} id="basic-nav-dropdown">
+                              <NavDropdown.Item href="#action/3.1">{ t("Action")}</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Menu;
